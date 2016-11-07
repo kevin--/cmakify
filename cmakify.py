660f155f6fc42b842e28ebe123cbe25ba846d788
Kevin Dixon <kevin.c.dixon@gmail.com>
2016-11-06 22:15:10 -0800

    WIP start writing cmakelists

--- cmakify.py
+++ cmakify.py
@@ -12,17 +12,23 @@ kHeaderKey = "headers"
 kSourceKey = "source"
 kCMakeListsKey = "cmake"
 
+kTargetType_Executable = "executable"
+kTargetType_Library = "library"
+
+kFileEntryMacro = "list(APPEND {0} {1})"
+
 def getArgs():
     import argparse
     parser = argparse.ArgumentParser(description="cmakify is used to generate a starting point for a CMake-based configuration of a pre-existing source tree.")
     parser.add_argument('path', help="the working path to generate CMakeLists.txt for")
     parser.add_argument('-f', '--flat', action='store_true', help="when specified, all directives will be placed in <path>/CMakeLists.txt. If not specified, a CMakeLists.txt will be placed in every subdirectory")
     parser.add_argument('--target-per-dir', action='store_true', help="when specified, each directory will be treated as a target")
-    parser.add_argument('--target-type', choices=["executable", "library"], help="when set, overrides the target type selection.")
+    parser.add_argument('--target-type', choices=[kTargetType_Executable, kTargetType_Library], help="when set, overrides the target type selection.")
     parser.add_argument('--header', nargs='*', default=['h','hpp','in'], help="specify the file extension(s) for header files.")
     parser.add_argument('--source', nargs='*', help="when set, specifies the source file extension(s). By default, everything that is not a header is considered")
     parser.add_argument('--project', help="the name of the project for CMake. If not set, the path's directory name will be used.")
     parser.add_argument('--ignore-dirs', nargs='*', default=['*.framework', 'lib', 'libs', 'bin'], help="ignore directories matching the specified pattern(s)")
+    parser.add_argument('--file-macro', default=kFileEntryMacro, help="for each file to be added, use this macro for the line in the generated CMakeLists.txt. {0} is replaced with the variable, {1} is the current file")
 
     return parser.parse_args()
 
@@ -104,6 +110,77 @@ def collectSourceFiles(inOutDirectoryContents, args, root, cwd, recurse):
             collectSourceFiles(directoryContents, args, nextRoot, nextCwd, True)
 
 
+def getCMakeLists_fileentry(args, variable, filename):
+    return args.file_macro.format(variable, filename)
+
+def writeCMakeLists_filegroup(args, directoryContents, root, cwd, variable, outputFile):
+
+    for s in directoryContents[kSourceKey][root][cwd]:
+        outputFile.write(getCMakeLists_fileentry(variable, s))
+
+    sourceGroupName = cwd.replace("/", "\\\\")
+    outputFile.write("source_group(" + sourceGroupName + " FILES ${" + variable + "})")
+
+def isRootTarget(args, root, cwd):
+    normRoot = os.path.normpath(root)
+    normCwd = os.path.normpath(os.path.join(root, cwd))
+    targetName = None
+    return normRoot == normCwd
+
+def getCMakeLists_targetname(args, root, cwd):
+    if isRootTarget(args, root, cwd):
+        if args.project == None:
+            targetName = os.path.dirname(os.path.abspath(root))
+        else:
+            targetName = args.project
+    else:
+        targetName = os.path.dirname(cwd)
+
+    if targetName == None:
+        raise Exception("failed to determine target name")
+
+    return targetName
+
+def getCMakeLists_targetmacro(args, root, cwd):
+    targetType = None
+    if args.target_type != None:
+        targetType = args.target_type
+    elif isRootTarget(args, root, cwd):
+        targetType = kTargetType_Executable
+    else:
+        targetType = kTargetType_Library
+
+    return "add_" + targetType + "({0} {1})"
+
+def getCMakeLists_targetentry(args, root, cwd, fileVariables):
+
+    targetName = getCMakeLists_targetname(args, root, cwd)
+    macro = getCMakeLists_targetmacro(args, root, cwd)
+
+    resolvedFileVariables = []
+    for v in fileVariables:
+        resolvedFileVariables.append("${" + v + "}")
+
+    return macro.format(targetName, " ".join(resolvedFileVariables))
+
+
+def writeCMakeLists_target(args, directoryContents, root, cwd, outputFile):
+
+    groups = []
+
+    normCwd = os.path.normpath(cwd)
+    normCwd = normCwd.replace("/", "_")
+    variableName = "FILES_{0}".format(normCwd)
+
+    writeCMakeLists_filegroup(args, directoryContents, root, cwd, variableName, outputFile)
+    outputFile.write(getCMakeLists_targetentry(args, root, cwd, [variableName]))
+
+
+def writeCMakeLists(args, directoryContents, root, cwd):
+    localFile = os.path.normpath(os.path.join(root, cwd, kCMakeListsFile))
+    with open(localFile, "w") as output:
+        writeCMakeLists_target(args, directoryContents, root, cwd, output)
+
 if __name__ == '__main__':
 
     args = getArgs()
@@ -121,6 +198,9 @@ if __name__ == '__main__':
 
     import pprint
     pprint.pprint(directoryContents)
+
+    writeCMakeLists(args, directoryContents, root, 'source')
+
 
 
 
